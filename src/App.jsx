dd7a92590cc797c021a84c28b8db77a5436d8941
Dhaneshwar Kumar <dhannesh@gmail.com>
2026-05-09 18:50:20 +0530

    add fragment

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,20 @@
 // the component name must start with capital letter
 // to stop server ctrl+c
 // to run project we use command "npm run dev" under project folder
-
 import Footer from "./Footer";
 import Home from "./Home";
+import { Bye, Hello } from "./Footer";
 
-const Hello = () => {
-  return <h2>This is first react component</h2>;
-};
-
-const Bye = () => {
-  return <h3>Bye bye user</h3>;
-};
 const App = () => {
   return (
-    <div>
+    <>
       <Home />
       <Hello />
       <h1>Hello React</h1>
       <Hello />
       <Bye />
       <Footer />
-    </div>
+    </>
   );
 };
 
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,13 @@
 const Footer = () => {
   return <h2>Footer</h2>;
 };
+
+export const Bye = () => {
+  return <h3>Bye bye user</h3>;
+};
+
+export const Hello = () => {
+  return <h2>This is first react component</h2>;
+};
+
 export default Footer;
