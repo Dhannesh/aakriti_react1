0e5173273adcf813dd9149d0054d9518f09bfd00
Dhaneshwar Kumar <dhannesh@gmail.com>
2026-05-09 17:55:24 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// when js function return html content then it is called component
+// it can be called by html tag with must closing tag
+// the component name must start with capital letter
+// to stop server ctrl+c
+// to run project we use command "npm run dev" under project folder
+const Hello = () => {
+  return <h2>This is first react component</h2>;
+};
+
+const Bye = () =>{
+  return <h3>Bye bye user</h3>
+}
+const App = () => {
+  return (
+    <div>
+      <Hello />
+      <h1>Hello React</h1>
+      <Hello />
+      <Bye/>
+    </div>
+  );
+};
+
+export default App;
