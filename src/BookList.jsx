8399a82760f43e4275f2d604aaf5bdfcd3923729
Dhaneshwar Kumar <dhannesh@gmail.com>
2026-05-10 18:44:31 +0530

    add books from object array

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -1,28 +1,12 @@
 import Book from "./Book";
+import { books } from "./data";
 
 const BookList = () => {
-  const book1 = {
-    title: "Let Us C++",
-    price: 433,
-    author: "Yashwant Kanitkar",
-  };
-  const book2 = {
-    title: "Let Us Java",
-    price: 493,
-    author: "Romesh Sharma",
-  };
-
   return (
     <section>
-      {/* <Book bookName={book1.title} writer={book1.author} price={book1.price} /> */}
-      <Book {...book1} /> 
-      <Book {...book2} />
-      {/* <Book title={book1.title} author={book1.author} price={book1.price}/> */}
-      {/* <Book bookName={title2} writer="Raghunandan" price="435" /> */}
-      {/* <Book />
-      <Book />
-      <Book />
-      <Book /> */}
+      {books.map((item) => (
+        <Book {...item} />
+      ))}
     </section>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const books = [
+  {
+    title: "Head First Java",
+    price: 834,
+    author: "Head First",
+  },
+  {
+    title: "Let Us C++",
+    price: 433,
+    author: "Yashwant Kanitkar",
+  },
+  {
+    title: "Let Us Java",
+    price: 493,
+    author: "Romesh Sharma",
+  },
+  {
+    title: "Let Us C#",
+    price: 893,
+    author: "Yogesh Kr Sharma",
+  },
+  {
+    title: "Let Us React",
+    price: 593,
+    author: "Reema Thareja",
+  },
+  {
+    title: "Let Us Python",
+    price: 493,
+    author: "Nirmala Raman Sharma",
+  },
+];
